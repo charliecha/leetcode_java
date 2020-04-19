75ebeb8990255ac66dc9ccb11bd2578e2022454f
Bean Cha <bean17.cha@gmail.com>
2020-04-19 15:47:18 +0800

    Update 466_L_GetMaxRepetitions.kt

--- src/main/kotlin/algorithm/466_L_GetMaxRepetitions.kt
+++ src/main/kotlin/algorithm/466_L_GetMaxRepetitions.kt
@@ -1,23 +1,49 @@
 package algorithm
 
 fun getMaxRepetitions(s1: String, n1: Int, s2: String, n2: Int): Int {
-    var repeatCnt = 0
-    var index2 = 0
-    for(i in 0 until n1) {
+    val repeatCnt = Array<Int>(n1 + 1) {0}
+    val nextIndex = Array<Int>(n1 + 1) {0}
+
+    var cnt = 0
+    var index = 0
+    for(i in 1 until n1 + 1) {
         for (j in 0 until s1.length) {
-            if (s1[j] == s2[index2]) {
-                if (index2 == s2.length - 1) {
-                    repeatCnt++
-                    index2 = 0
+            if (s1[j] == s2[index]) {
+                if (index == s2.length - 1) {
+                    cnt++
+                    index = 0
                 } else {
-                    index2++
+                    index++
                 }
             }
         }
+
+        repeatCnt[i] = cnt
+        nextIndex[i] = index
+
+        for (k in 1 until i) {
+            // 重复模式出现
+            if (nextIndex[k] == nextIndex[i]) {
+                val interval = i - k
+                val r = (n1 - k + 1) / interval
+                val repeats = r * (repeatCnt[i] - repeatCnt[k])
+                val remains = repeatCnt[n1 - r * interval]
+                return (repeats + remains) / n2
+            }
+        }
     }
-    return repeatCnt / n2
+
+//    println(Arrays.toString(repeatCnt))
+//    println(Arrays.toString(nextIndex))
+    return cnt / n2
 }
 
 fun main() {
-    getMaxRepetitions("aaa", 3, "aa", 1)
+//    "bacaba"
+//    3
+//    "abacab"
+//    1
+
+    println(getMaxRepetitions("aaa", 3, "aa", 2))
+//    println(getMaxRepetitions("bacaba", 3, "abacab", 2))
 }
